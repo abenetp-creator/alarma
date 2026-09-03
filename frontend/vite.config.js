3540888296830cd58c37d264a5e91f819d42218c
abenetp-creator <abenetp@gmail.com>
2026-09-03 13:56:11 +0200

    Afegit suport per a iOS 15 a vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'safari >= 13', 'ios >= 13'],
+    }),
+  ],
+  build: {
+    target: ['es2015', 'safari13'],
+  },
 })
